81b7abdc1ff48f26b0b347299d1777ca16e6ca90
mk928000000 <mk928000000@gmail.com>
2022-08-02 08:22:48 +0900

    use padStart function on minutes

--- javaScript/js/clock.js
+++ javaScript/js/clock.js
@@ -5,7 +5,7 @@ function getClock(){
     //date.getHours() 의 반환값은 number 이므로 String 함수인 padStart 를 쓸 수 없으나,
     //String(내용) 으로 감싸주면 String 으로 변환된다.
     const hours = String (date.getHours()).padStart(2,"0");
-    const minutes = String (date.getMinutes());
+    const minutes = String (date.getMinutes()).padStart(2,"0");
     const seconds = String (date.getSeconds()).padStart(2,"0");
 
     clock.innerHTML = `${hours}:${minutes}:${seconds}`;
