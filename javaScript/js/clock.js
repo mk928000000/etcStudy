f581a69dd865c138a511d8930e658e6fc54faa84
mk928000000 <mk928000000@gmail.com>
2022-08-02 08:02:12 +0900

    makeClock

--- javaScript/js/clock.js
+++ javaScript/js/clock.js
@@ -1,3 +1,11 @@
 const clock =document.querySelector("h2#clock");
 
-clock.innerText="lalalalal";
+function getClock(){
+    const date = new Date();
+    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+
+getClock();
+//Every second "Hello" in the console.
+setInterval(getClock, 1000);
