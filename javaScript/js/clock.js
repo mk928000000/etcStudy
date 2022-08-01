1e5ee3e8cf62ab1dc8b99901c2934ce8e66916b4
mk928000000 <mk928000000@gmail.com>
2022-08-02 08:20:25 +0900

    make String, use padStart function

--- javaScript/js/clock.js
+++ javaScript/js/clock.js
@@ -2,10 +2,16 @@ const clock =document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    //date.getHours() 의 반환값은 number 이므로 String 함수인 padStart 를 쓸 수 없으나,
+    //String(내용) 으로 감싸주면 String 으로 변환된다.
+    const hours = String (date.getHours()).padStart(2,"0");
+    const minutes = String (date.getMinutes());
+    const seconds = String (date.getSeconds()).padStart(2,"0");
+
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
-
+//call this immediately.
 getClock();
 //Every second "Hello" in the console.
 setInterval(getClock, 1000);
